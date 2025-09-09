f7aebcc298d95a5c67c1f43bdeacea269f2de16c
Arunkumarravik <arunkumarravi60@gmail.com>
2025-09-09 18:04:32 +0530

    Sixth web copy

--- dsa_solve_problem_script.js
+++ dsa_solve_problem_script.js
@@ -1,17 +1,69 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 document.getElementById('get-problem-btn').addEventListener('click', async () => {
     const type = document.getElementById('problem-type').value;
     const topic = document.getElementById('dsa-topic').value;
-
+    const difficulty = document.getElementById('problem-difficulty').value;
+    const user_id=1
+    const user_name="Arun kumar R"
+    const problem_id=getRandomInt(1, 100000)
+    const today=new Date()
+    const today_date=today.toLocaleDateString()
     // Call backend API
-    const response = await fetch('/api/get_problem', {
+
+    title_payload={
+        "user_id" :  user_id , 
+        "user_name": user_name ,
+        "problem_id": problem_id,
+        "topic" : topic ,
+        "category" : type ,
+        "difficulty" :  difficulty,
+        "problem_creation_date" : today_date.toString()
+    }
+
+    payload={
+        "event" :  "get-title",
+        "type" :  "main",
+        "data" :  title_payload
+    }
+    const response = await fetch('https://dsadiscobackend-214580149659.us-west1.run.app', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ type, topic })
+        body: JSON.stringify(payload)
     });
     const data = await response.json();
 
-    document.getElementById('problem-description').innerText = data.description;
+    console.log(data)
+
+    // Create route button dynamically
+    // Create wrapper div
+    const problemDiv = document.createElement("div");
+    problemDiv.classList.add("problem-row");
+
+    // Title text
+    const titleSpan = document.createElement("span");
+    titleSpan.innerText = `${data.title} (${topic})`;
+
+    // Solve button
+    const editorBtn = document.createElement("button");
+    editorBtn.classList.add("solve-btn");
+    editorBtn.innerText = "Solve";
+    editorBtn.addEventListener("click", () => {
+        const url = `dsa_problem_editor_page.html?title=${encodeURIComponent(data.title)}&topic=${encodeURIComponent(topic)}`;
+        window.location.href = url;
+    });
+
+    // Append title + button
+    problemDiv.appendChild(titleSpan);
+    problemDiv.appendChild(editorBtn);
+
+    // Add to container
+    document.getElementById("solve-prb").appendChild(problemDiv);
+
 });
+
 
 // Toggle chat sidebar
 document.getElementById('toggle-chat').addEventListener('click', () => {
